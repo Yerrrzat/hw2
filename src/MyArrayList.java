a8071f947ae8254bb80e61732286146d46de98c0
Yerzat <erzatnumnberone@gmail.com>
2025-04-14 00:11:04 +0500

    add full insertion/removal/get methods to MyArrayList

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -17,18 +17,45 @@ public class MyArrayList<T> implements MyList<T> {
         data[size++] = item;
     }
 
-    private void grow() {
-        Object[] newData = new Object[data.length * 2];
-        for (int i = 0; i < data.length; i++) {
-            newData[i] = data[i];
+    @Override
+    public void add(int index, T item) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
         }
-        data = newData;
+        if (size == data.length) {
+            grow();
+        }
+        for (int i = size; i > index; i--) {
+            data[i] = data[i - 1];
+        }
+        data[index] = item;
+        size++;
+    }
+
+    @Override
+    public void addFirst(T item) {
+        add(0, item);
+    }
+
+    @Override
+    public void addLast(T item) {
+        add(item);
     }
 
     @Override
     public T get(int index) {
         checkIndex(index);
         return (T) data[index];
+    }
+
+    @Override
+    public T getFirst() {
+        return get(0);
+    }
+
+    @Override
+    public T getLast() {
+        return get(size - 1);
     }
 
     @Override
@@ -42,6 +69,16 @@ public class MyArrayList<T> implements MyList<T> {
     }
 
     @Override
+    public void removeFirst() {
+        remove(0);
+    }
+
+    @Override
+    public void removeLast() {
+        remove(size - 1);
+    }
+
+    @Override
     public void clear() {
         for (int i = 0; i < size; i++) {
             data[i] = null;
@@ -50,13 +87,27 @@ public class MyArrayList<T> implements MyList<T> {
     }
 
     @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
     public int size() {
         return size;
     }
 
-    @Override
-    public boolean isEmpty() {
-        return size == 0;
+    private void grow() {
+        Object[] newData = new Object[data.length * 2];
+        for (int i = 0; i < data.length; i++) {
+            newData[i] = data[i];
+        }
+        data = newData;
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
     }
 
     @Override
@@ -76,9 +127,16 @@ public class MyArrayList<T> implements MyList<T> {
         };
     }
 
-    private void checkIndex(int index) {
-        if (index < 0 || index >= size) {
-            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
-        }
-    }
+    @Override
+    public void set(int index, T item) {}
+    @Override
+    public void sort() {}
+    @Override
+    public int indexOf(Object object) { return -1; }
+    @Override
+    public int lastIndexOf(Object object) { return -1; }
+    @Override
+    public boolean exists(Object object) { return false; }
+    @Override
+    public Object[] toArray() { return null; }
 }
--- src/MyList.java
+++ src/MyList.java
@@ -3,9 +3,38 @@ import java.util.Iterator;
 
 public interface MyList<T> extends Iterable<T> {
     void add(T item);
+
+    void add(int index, T item);
+
+    void addFirst(T item);
+
+    void addLast(T item);
+
     T get(int index);
+
+    T getFirst();
+
+    T getLast();
+
     void remove(int index);
     int size();
+
+    void removeFirst();
+
+    void removeLast();
+
     void clear();
     boolean isEmpty();
+
+    void set(int index, T item);
+
+    void sort();
+
+    int indexOf(Object object);
+
+    int lastIndexOf(Object object);
+
+    boolean exists(Object object);
+
+    Object[] toArray();
 }
